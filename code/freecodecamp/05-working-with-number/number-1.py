7f28a794e46a38153fdcb008362d85a2f4ab66c4
imamfarisi19 <edwardindo1@gmail.com>
2021-06-12 23:42:58 +0700

    function of math

---
+++ code/freecodecamp/05-working-with-number/number-1.py
@@ -0,0 +1,45 @@
+from math import * 
+
+# print number as a string 
+my_num = 5 
+print(str(my_num) + " my favorite number") 
+
+# error! make sure you have all value same like number and number or string and string, convert it to string! 
+#my_num = 5 
+#print(my_num + " my favorite number") 
+
+# print the minus number 
+my_num = -5 
+print(abs(my_num)) 
+
+# power function 
+my_num = -5 
+print(pow(3, 2)) 
+
+# more power function
+print(pow(4, 6)) 
+
+# return  the highest value 
+my_num = -5 
+print(max(4, 6)) 
+
+# return the lowest value 
+my_num = -5 
+print(min(4, 6)) 
+
+# return round value of the number and round it to the close round number
+my_num = -5 
+print(round(3.7)) 
+
+# return round value of the number and make it round to the lowest
+my_num = -5
+print(floor(3.7)) 
+
+# return round value of the number and make it round to the heighst
+my_num = -5 
+print(ceil(3.7)) 
+
+# calculate square root of the given number and return the value 
+my_num = -5 
+print(sqrt(36)) 
+
