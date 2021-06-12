d7e29d825846f3993504726c8a56754e4a81c7a6
imamfarisi19 <edwardindo1@gmail.com>
2021-06-12 23:39:22 +0700

    search a character in string

---
+++ code/freecodecamp/04-working-with-string/string-6.py
@@ -0,0 +1,12 @@
+phrase = "Giraffe Academy" 
+print(phrase.index("a")) 
+
+phrase = "Giraffe Academy" 
+print(phrase.index("G")) 
+
+phrase = "Giraffe Academy" 
+print(phrase.index("Acad")) 
+
+phrase = "Giraffe Academy" 
+print(phrase.index("z")) 
+
