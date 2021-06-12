5350408069e4e593889fede1150079bd1fd561c3
imamfarisi19 <edwardindo1@gmail.com>
2021-06-13 00:23:54 +0700

    dictionaries for key-value pairs.

---
+++ code/freecodecamp/17-dictionaries/dictionaries-0.py
@@ -0,0 +1,17 @@
+monthConversion = { 
+    0: "January", 
+    "Feb": "February", 
+    "Mar": "March", 
+    "Apr": "April", 
+    "May": "May", 
+    "Jun": "June", 
+    "Jul": "July", 
+    "Aug": "August", 
+    "Sep": "September", 
+    "Oct": "October", 
+    "Nov": "November", 
+    "Dec": "December", 
+} 
+
+print(monthConversion.get("Luv", "Not a valid Key")) 
+
