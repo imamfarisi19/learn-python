bcfc3389130567afdf7d40f3ff58181f545cfe91
imamfarisi19 <edwardindo1@gmail.com>
2021-06-22 21:40:56 +0700

    no errors and we can play it multiplayer

--- code/freecodecamp/12-beginner-python-projects-coding-course/06-tic-tac-toe/player.py
+++ code/freecodecamp/12-beginner-python-projects-coding-course/06-tic-tac-toe/player.py
@@ -8,33 +8,38 @@ class Player:
 
     # we want all players to get their next move given a game 
     def get_move(self, game): 
-        pass 
+        # get a random valid spot for our next move 
+        square = random.choice(game.available_moves()) 
+        return square 
 
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
+class RandomComputerPlayer(Player): 
+    def __init__(self, letter): 
         super().__init__(letter) 
 
     def get_move(self, game): 
+        # get a random valid spot for our next move 
         square = random.choice(game.available_moves()) 
+        return square 
 
-class HumanPlayer(Player):
+class HumanPlayer(Player): 
     def __init__(self, letter): 
-        super().__init__(letter)
+        super().__init__(letter) 
 
-    def get_move(self,game):
-        valid_square = False
+    def get_move(self, game): 
+        valid_square = False 
         val = None 
         while not valid_square: 
-            square = input(self.letter + '\'s turn. Input move (0-8):')
+            square = input(self.letter + '\'s turn. Input move (0-8):') 
             # we're going to check that this is a correct value by trying to cast 
-            # it to an integer, and if it's not, then we say its invalid
+            # it to an integer, and if it's not, then we say its invalid 
             # if that spot is not available on the board, we also say its invalid 
             try: 
                 val = int(square) 
                 if val not in game.available_moves(): 
                     raise ValueError 
-                valid_square = True # if these are successful, then yay! 
-            except ValueError:
-                print('Invalid square. Try again.')
+                valid_square = True # if these are succesful, then yay! 
+            except: 
+                print('Invalid square. Try again.') 
 
-        return val
+        return val 
+
