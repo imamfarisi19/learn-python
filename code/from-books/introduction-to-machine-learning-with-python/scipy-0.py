066d37341c30c92a55b9c65bb4fb5de2828ae2f1
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 01:43:00 +0800

    Scipy is a collection of functions for scientific computing in Python

---
+++ code/from-books/introduction-to-machine-learning-with-python/scipy-0.py
@@ -0,0 +1,7 @@
+import numpy as np 
+from scipy import sparse 
+
+# create a 20 NumPy array with a diagonal of ones, and zeros everywhere else
+eye = np.eye(4) 
+print("NumPy array:\n{}",format(eye)) 
+
