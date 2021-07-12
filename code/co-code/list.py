d41748f030e1f808a5856dfed5513ed02ed2c78c
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 22:51:19 +0800

    list example

---
+++ code/co-code/list.py
@@ -0,0 +1,21 @@
+mylist = ["apple", 12, True, 12, 12] 
+mylist[2] = 13
+#print(mylist)
+
+mytuples = ("apple", 12, True, True)
+#print(mytuples)
+
+#mytuple[2] = 23
+#print(mytuples)
+
+myset = {"apple", 12, True, False} 
+#print(myset)
+
+thisdict = { 
+    "name": "Farisi", 
+    "age": 20, 
+    3: "a three",
+    True: "this is True"
+}
+#print(thisdict[True])
+
