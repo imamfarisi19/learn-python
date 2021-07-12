366edbae56d883084a2ae012020931fd0bb98ea9
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 22:51:19 +0800

    if-statements example

---
+++ code/co-code/if-statements.py
@@ -0,0 +1,25 @@
+farisi = 
+
+if farisi < 10: 
+    print(farisi) 
+    farisi = farisi + 1 
+    print(farisi)  
+    if farisi == 7:
+        print("its really accurate")
+    else:
+        print("no accurately")
+
+elif farisi == 11:    
+    print(farisi) 
+
+elif farisi > 12:
+    print(farisi) 
+    farisi = farisi + 2
+    print(farisi) 
+
+else: 
+    print("Variable is greater") 
+
+
+    
+
