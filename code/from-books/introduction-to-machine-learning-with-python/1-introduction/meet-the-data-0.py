263e70bdf35b1e138fc7f44593956ae7867867d1
imamfarisi19 <edwardindo1@gmail.com>
2021-07-13 02:22:48 +0800

    We show the beginning of the description here

--- code/from-books/introduction-to-machine-learning-with-python/1-introduction/meet-the-data-0.py
+++ code/from-books/introduction-to-machine-learning-with-python/1-introduction/meet-the-data-0.py
@@ -2,3 +2,6 @@ from sklearn.datasets import load_iris
 iris_dataset = load_iris() 
 
 print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+
+print(iris_dataset['DESCR'][:193] + "\n...")
+
