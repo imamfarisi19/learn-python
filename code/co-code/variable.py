7bd62e06ee95cf1b0d9d65768b7c7312f5a644bc
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 22:51:20 +0800

    variable example

---
+++ code/co-code/variable.py
@@ -0,0 +1,18 @@
+ひかる = 11 + 12 # 23 
+farisi = 10 - ひかる # -13 
+
+ひかる = ひかる * 2 # 46 
+ひかる = ひかる / farisi # 46 / -13 = -3.5384615384615383
+
+print(ひかる) # -3.5384615384615383
+print(farisi) # 21, -1 
+
+
+
+
+
+
+# variable is container safe your value 
+# number, 
+#  string -> kinda of text, "" or '' backticks `` 
+# boolean True or false 
