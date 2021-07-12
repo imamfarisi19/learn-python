16089029f29f0f4682985cc5054b196571607dc7
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 22:51:46 +0800

    const usage

---
+++ code/co-code/test.py
@@ -0,0 +1,10 @@
+# import module
+from pconst import const
+  
+# declare constants
+const.LANGUAGE = "PYTHON"
+const.COMPANY_NAME = 'GFG'
+  
+# display 
+print(const.LANGUAGE) 
+print(const.COMPANY_NAME) 
