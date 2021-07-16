6095c95d7741b7c7024ac3e86a2a49cb17df30e6
imamfarisi19 <edwardindo1@gmail.com>
2021-07-16 16:20:50 +0800

    To illustrate regression algorithms, we will use the synthetic wave dataset.

---
+++ code/from-books/introduction-to-machine-learning-with-python/2-supervised-learning/some-sample-datasets-1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn 
+
+# generate dataset 
+X, y = mglearn.datasets.make_forge()
+# plot dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc=4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+print("X.shape: {}".format(X.shape))
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+
